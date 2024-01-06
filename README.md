119f3367b0693fbe3cca9d167c3629f1059f2e30
Zeus <elderfieldzeus24@gmail.com>
2024-01-06 19:23:45 +0800

    First Linux Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # linuxProjects
+Projects made by me on an Ubuntu Linux operating system.
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char name[100];
+    printf("Hello, World\n");
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("HELLO %s", name);
+    return 0;
+}
